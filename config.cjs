8a55527a9f408f4b39a1e49ac14ead773f54b0e2
karmael161 <ekarmael@gmail.com>
2024-10-11 17:29:09 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "put your session Id",
+  SESSION_ID: process.env.SESSION_ID || "Bmwmd$dtx7nWCF",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : false, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "©Fredie",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255745086279",
+  OWNER_NAME: process.env.OWNER_NAME || "𝐋𝚯𝚪𝐃 𝐃𝚫𝚪𝐊 𝐒𝚫𝐒𝚫𝐊𝚰",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "237672080608",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : true, 
 };
